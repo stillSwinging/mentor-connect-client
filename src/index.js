3f7f4dd57e91e743c72afd20e07b9bd556f8b9e2
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-06-17 19:48:43 -0700

    added login and logout js files updated index.js with auth0 info

--- src/index.js
+++ src/index.js
@@ -17,17 +17,3 @@ root.render(
     </Auth0Provider>
   </>
 );
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <>
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   </>
-// );
