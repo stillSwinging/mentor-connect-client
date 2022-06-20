7cfa8e2cd945479b6e9ad381af0be4f3dce3405f
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-06-20 17:10:49 -0400

    cleared up some minor errors appearing on terminal

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import Header from './Components/Header';
 import Body from './Components/Body';
 import Footer from './Components/Footer';
 import Simple from './Components/Simple';
+import axios from 'axios';  // for the constructor below (VP 6/19/2022)
+
 class App extends React.Component {
+
+  constructor(props) { // I think this constructor actually belongs on the userInfo card that MQR is coding. Move as appropriate. (VP 6/19/2022)
+    super(props);
+    this.state = {
+      resultingUserList: [] // (VP 6/19/2022)
+    };
+  }
+
+  getMentors = async () => { // (VP 6/19/2022)
+    const url = process.env.REACT_APP_API;
+    const resultingUserList = await axios.get(`${url}/userInfo?type=mentor`);
+    console.log(resultingUserList);
+  };
+
+  getStudents = async () => { // (VP 6/19/2022)
+    const url = process.env.REACT_APP_API;
+    const resultingUserList = await axios.get(`${url}/userInfo?type=student`);
+    console.log(resultingUserList);
+  };
+
   render() {
     return (
-      <Router>
-        <Header />
-        <Body />
-        <Simple />
-        <Footer />
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-        </Routes>
-      </Router>
+      <>
+        <Router >
+          <Header />
+          <Body />
+          <Simple />
+          <Footer />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Auth0Provider } from "@auth0/auth0-react";
+import {Auth0Provider} from '@auth0/auth0-react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
