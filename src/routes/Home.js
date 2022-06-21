dcceed00e5ee7f7f8c95cba9797c4899be334ea3
Provida Powell <102064281+Vida-1@users.noreply.github.com>
2022-06-20 20:30:55 -0400

    Revert "Tb"

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import Body from '../Components/Body';
-import Simple from '../Components/Simple';
 
 class Home extends React.Component {
   render() {
     return (
-      // adding fragments so i can import the simple.js and body, this is so the home page photos do not render on every single other component -Tanesha Brester
-      <>
-        <div>
-          <h1>Mentor Matcher</h1>
-        </div>
-        {/* if you want to re-arrange the photos you can swap the order of the Body and Simple tags - Tanesha Brester */}
-        <Body></Body>
-        <Simple></Simple>
-  
-      
-      </>
+      <div>
+        <h1>Mentor Matcher</h1>
+      </div>
     );
   }
 }
